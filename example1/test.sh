e5fc76e9a4793febd2a4a3a5e7205d6a0ce27eda
benchenglian <benchenglian@foxmai.com>
2018-02-01 21:39:52 +0800

    ok

---
+++ example1/test.sh
@@ -0,0 +1,12 @@
+### this is test scripts #
+export ANDROID_HOME=/Users/lianfeng/profile/android-sdk_r24.4.1-macosx/android-sdk-macosx
+export PATH=${PATH}:${ANDROID_HOME}/tools
+export PATH=${PATH}:${ANDROID_HOME}/platform-tools
+#build
+./gradlew clean
+./gradlew assembleDebug installDebug
+./gradlew assembleDebugAndroidTest installDebugAndroidTest
+
+#test
+
+adb shell am instrument -w -r   -e debug false -e class com.application.example1.ExampleInstrumentedTest com.application.example1.test/android.support.test.runner.AndroidJUnitRunner
